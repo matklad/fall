cc57205efe0481736a7d66dbb5a254c93386cd66
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-03-30 22:36:34 +0300

    More thorough test

--- fall_test/tests/json.rs
+++ fall_test/tests/json.rs
@@ -2,11 +2,11 @@ extern crate fall_test;
 extern crate fall_tree;
 
 use fall_test::{json, match_ast};
-use fall_tree::dump_file_ws;
+use fall_tree::dump_file;
 
 
 fn ast(code: &str) -> String {
-    dump_file_ws(&json::parse(code.to_owned()))
+    dump_file(&json::parse(code.to_owned()))
 }
 
 #[test]
@@ -15,26 +15,189 @@ fn obj() {
 FILE
   OBJECT
     LBRACE "{"
-    WHITESPACE " "
     FIELD
       STRING "\"foo\""
       COLON ":"
-      WHITESPACE " "
       ARRAY
         LBRACK "["
         PRIMITIVE
           NUMBER "1"
         COMMA ","
-        WHITESPACE " "
         PRIMITIVE
           NUMBER "2"
         COMMA ","
-        WHITESPACE " "
         PRIMITIVE
           NUMBER "3"
         RBRACK "]"
-    WHITESPACE " "
     RBRACE "}"
 "#);
 }
+
+#[test]
+fn example() {
+    match_ast(&ast(r#"
+{"widget": {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}}"#), r#"
+FILE
+  OBJECT
+    LBRACE "{"
+    FIELD
+      STRING "\"widget\""
+      COLON ":"
+      OBJECT
+        LBRACE "{"
+        FIELD
+          STRING "\"debug\""
+          COLON ":"
+          PRIMITIVE
+            STRING "\"on\""
+        COMMA ","
+        FIELD
+          STRING "\"window\""
+          COLON ":"
+          OBJECT
+            LBRACE "{"
+            FIELD
+              STRING "\"title\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"Sample Konfabulator Widget\""
+            COMMA ","
+            FIELD
+              STRING "\"name\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"main_window\""
+            COMMA ","
+            FIELD
+              STRING "\"width\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "500"
+            COMMA ","
+            FIELD
+              STRING "\"height\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "500"
+            RBRACE "}"
+        COMMA ","
+        FIELD
+          STRING "\"image\""
+          COLON ":"
+          OBJECT
+            LBRACE "{"
+            FIELD
+              STRING "\"src\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"Images/Sun.png\""
+            COMMA ","
+            FIELD
+              STRING "\"name\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"sun1\""
+            COMMA ","
+            FIELD
+              STRING "\"hOffset\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "250"
+            COMMA ","
+            FIELD
+              STRING "\"vOffset\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "250"
+            COMMA ","
+            FIELD
+              STRING "\"alignment\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"center\""
+            RBRACE "}"
+        COMMA ","
+        FIELD
+          STRING "\"text\""
+          COLON ":"
+          OBJECT
+            LBRACE "{"
+            FIELD
+              STRING "\"data\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"Click Here\""
+            COMMA ","
+            FIELD
+              STRING "\"size\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "36"
+            COMMA ","
+            FIELD
+              STRING "\"style\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"bold\""
+            COMMA ","
+            FIELD
+              STRING "\"name\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"text1\""
+            COMMA ","
+            FIELD
+              STRING "\"hOffset\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "250"
+            COMMA ","
+            FIELD
+              STRING "\"vOffset\""
+              COLON ":"
+              PRIMITIVE
+                NUMBER "100"
+            COMMA ","
+            FIELD
+              STRING "\"alignment\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"center\""
+            COMMA ","
+            FIELD
+              STRING "\"onMouseUp\""
+              COLON ":"
+              PRIMITIVE
+                STRING "\"sun1.opacity = (sun1.opacity / 100) * 90;\""
+            RBRACE "}"
+        RBRACE "}"
+    RBRACE "}"
+"#)
+}
+
 
