34cf3f40cb5cdc9b7d2fb5665e9fc77d928f4ff2
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-04-01 22:58:49 +0300

    Move ast lifting

--- fall_gen/src/ast.rs
+++ fall_gen/src/ast.rs
@@ -1,3 +1,8 @@
+use fall_tree::Node;
+use fall_tree::search::{child_of_type, child_of_type_exn, children_of_type};
+
+use syntax::*;
+
 #[derive(Debug)]
 pub struct Grammar {
     pub node_types: Vec<String>,
@@ -28,4 +33,98 @@ pub struct Alt {
 pub enum Part {
     Rule(String),
     Rep(Alt)
+}
+
+
+#[derive(Debug)]
+pub struct LiftError(String);
+
+impl ::std::error::Error for LiftError {
+    fn description(&self) -> &str {
+        &self.0
+    }
+}
+
+impl ::std::fmt::Display for LiftError {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        f.write_str("Syntax error: ")?;
+        f.write_str(&self.0)
+    }
+}
+
+fn error<S: Into<String>>(msg: S) -> LiftError { LiftError(msg.into()) }
+
+type Result<T> = ::std::result::Result<T, LiftError>;
+
+
+pub fn lift(node: Node) -> Result<Grammar> {
+    let node_types = child_of_type_exn(node, NODES_DEF);
+    let lex_rules = child_of_type_exn(node, TOKENIZER_DEF);
+    let syn_rules = children_of_type(node, RULE_DEF);
+
+    let g = Grammar {
+        node_types: lift_node_types(node_types)?,
+        lex_rules: lift_lex_rules(lex_rules)?,
+        syn_rules: syn_rules.map(lift_syn_rule).collect::<::std::result::Result<Vec<_>, _>>()?
+    };
+
+    Ok(g)
+}
+
+fn lift_node_types(node: Node) -> Result<Vec<String>> {
+    Ok(children_of_type(node, IDENT)
+        .map(node_text)
+        .collect())
+}
+
+fn lift_lex_rules(node: Node) -> Result<Vec<LexRule>> {
+    children_of_type(node, TOKEN_DEF)
+        .map(lift_lex_rule)
+        .collect()
+}
+
+fn lift_lex_rule(node: Node) -> Result<LexRule> {
+    let ty = node_text(
+        child_of_type(node, IDENT).ok_or(error("Missing name in rule"))?
+    );
+
+    let mut pats = children_of_type(node, STRING);
+    let re = node_text(
+        pats.next().ok_or(error(format!("Missing pattern in rule {:?}", node.text())))?
+    );
+    let f = pats.next().map(|n| {
+        let t = n.text();
+        assert!(t.starts_with("\"") && t.ends_with("\""));
+        t[1..t.len() - 1].to_owned()
+    });
+    assert!(pats.next().is_none());
+    Ok(LexRule { ty: ty, re: re, f: f })
+}
+
+fn lift_syn_rule(node: Node) -> Result<SynRule> {
+    Ok(SynRule {
+        name: node_text(child_of_type_exn(node, IDENT)),
+        alts: children_of_type(node, ALT).map(lift_alt).collect(),
+    })
+}
+
+fn lift_alt(node: Node) -> Alt {
+    Alt {
+        parts: children_of_type(node, PART)
+            .filter(|n| n.text() != "<commit>")
+            .map(lift_part).collect(),
+        commit: children_of_type(node, PART).position(|n| n.text() == "<commit>"),
+    }
+}
+
+fn lift_part(node: Node) -> Part {
+    if let Some(id) = child_of_type(node, IDENT) {
+        Part::Rule(node_text(id))
+    } else {
+        Part::Rep(lift_alt(child_of_type_exn(node, ALT)))
+    }
+}
+
+fn node_text(node: Node) -> String {
+    node.text().to_owned()
 }
--- fall_gen/src/lib.rs
+++ fall_gen/src/lib.rs
@@ -1,101 +1,22 @@
 extern crate fall_tree;
 extern crate fall_parse;
 
-use fall_tree::{dump_file, Node};
-use fall_tree::search::{child_of_type, child_of_type_exn, children_of_type};
+use fall_tree::dump_file;
 
 mod syntax;
 mod ast;
 
-use self::syntax::*;
-
 mod generator;
 
-pub use ast::{Grammar, LexRule, SynRule, Alt, Part};
+pub use ast::{Grammar, LexRule, SynRule, Alt, Part, LiftError};
 
-#[derive(Debug)]
-pub struct Error(String);
-
-impl ::std::error::Error for Error {
-    fn description(&self) -> &str {
-        &self.0
-    }
-}
-
-impl ::std::fmt::Display for Error {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.write_str("Syntax error: ")?;
-        f.write_str(&self.0)
-    }
-}
-
-fn error<S: Into<String>>(msg: S) -> Error { Error(msg.into()) }
 
 pub fn debug(text: &str) -> String {
     dump_file(&syntax::parse(text.to_owned()))
 }
 
-pub fn parse(text: &str) -> Result<Grammar, Error> {
+pub fn parse(text: &str) -> Result<Grammar, LiftError> {
     let file = syntax::parse(text.to_owned());
     let root = file.root();
-
-    let nodes = child_of_type_exn(root, NODES_DEF);
-    let tokenizer = child_of_type_exn(root, TOKENIZER_DEF);
-    let syn_rules = children_of_type(root, RULE_DEF);
-
-    let node_types = children_of_type(nodes, IDENT)
-        .map(|n| n.text().to_owned())
-        .collect();
-
-    let lex_rules = children_of_type(tokenizer, TOKEN_DEF)
-        .map(|rule| -> Result<LexRule, Error> {
-            let ty = child_of_type(rule, IDENT)
-                .ok_or(error("Missing name in rule"))?
-                .text().to_owned();
-            let mut pats = children_of_type(rule, STRING);
-            let re = pats.next()
-                .ok_or(error(format!("Missing pattern in rule {:?}", rule.text())))?;
-            let f = pats.next().map(|n| {
-                let t = n.text();
-                t[1..t.len() - 1].to_owned()
-            });
-            Ok(LexRule { ty: ty, re: re.text().to_owned(), f: f })
-        })
-        .collect::<Result<Vec<_>, Error>>()?;
-
-    let syn_rules = syn_rules.map(parse_syn_rule).collect();
-
-    let g = Grammar {
-        node_types: node_types,
-        lex_rules: lex_rules,
-        syn_rules: syn_rules
-    };
-
-    Ok(g)
-}
-
-
-fn parse_syn_rule(rule: Node) -> SynRule {
-    SynRule {
-        name: child_of_type_exn(rule, IDENT).text().to_string(),
-        alts: children_of_type(rule, ALT).map(parse_alt).collect(),
-    }
-}
-
-fn parse_alt(alt: Node) -> Alt {
-    Alt {
-        parts: children_of_type(alt, PART)
-            .filter(|n| n.text() != "<commit>")
-            .map(parse_part).collect(),
-        commit: children_of_type(alt, PART).position(|n| n.text() == "<commit>"),
-    }
-}
-
-fn parse_part(node: Node) -> Part {
-    if let Some(id) = child_of_type(node, IDENT) {
-        Part::Rule(id.text().to_owned())
-    } else {
-        Part::Rep(parse_alt(child_of_type_exn(node, ALT)))
-    }
-
+    ast::lift(root)
 }
