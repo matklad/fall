3e467cdad24e2765ebd3a235bd0eaef183f230c5
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-06-24 00:16:26 +0300

    A bit more Rust

--- lang/rust/src/syntax.rs
+++ lang/rust/src/syntax.rs
@@ -14,26 +14,29 @@ pub const KW_PUB: NodeType = NodeType(108);
 pub const KW_LET: NodeType = NodeType(109);
 pub const STRUCT: NodeType = NodeType(110);
 pub const FN: NodeType = NodeType(111);
-pub const IDENT: NodeType = NodeType(112);
-pub const NUMBER: NodeType = NodeType(113);
-pub const FILE: NodeType = NodeType(114);
-pub const FN_DEF: NodeType = NodeType(115);
-pub const STRUCT_DEF: NodeType = NodeType(116);
-pub const STRUCT_FIELD: NodeType = NodeType(117);
-pub const TYPE: NodeType = NodeType(118);
-pub const BLOCK_EXPR: NodeType = NodeType(119);
-pub const STMT: NodeType = NodeType(120);
-pub const PATTERN: NodeType = NodeType(121);
-pub const EXPR: NodeType = NodeType(122);
+pub const USE: NodeType = NodeType(112);
+pub const IDENT: NodeType = NodeType(113);
+pub const NUMBER: NodeType = NodeType(114);
+pub const FILE: NodeType = NodeType(115);
+pub const USE_DECL: NodeType = NodeType(116);
+pub const FN_DEF: NodeType = NodeType(117);
+pub const STRUCT_DEF: NodeType = NodeType(118);
+pub const STRUCT_FIELD: NodeType = NodeType(119);
+pub const PATH: NodeType = NodeType(120);
+pub const TYPE: NodeType = NodeType(121);
+pub const BLOCK_EXPR: NodeType = NodeType(122);
+pub const STMT: NodeType = NodeType(123);
+pub const PATTERN: NodeType = NodeType(124);
+pub const EXPR: NodeType = NodeType(125);
 
 lazy_static! {
     pub static ref LANG: Language = {
         use fall_parse::{LexRule, SynRule, Parser};
         const ALL_NODE_TYPES: &[NodeType] = &[
             ERROR, WHITESPACE,
-            LPAREN, RPAREN, LBRACE, RBRACE, EQ, SEMI, COLON, COMMA, KW_PUB, KW_LET, STRUCT, FN, IDENT, NUMBER, FILE, FN_DEF, STRUCT_DEF, STRUCT_FIELD, TYPE, BLOCK_EXPR, STMT, PATTERN, EXPR,
+            LPAREN, RPAREN, LBRACE, RBRACE, EQ, SEMI, COLON, COMMA, KW_PUB, KW_LET, STRUCT, FN, USE, IDENT, NUMBER, FILE, USE_DECL, FN_DEF, STRUCT_DEF, STRUCT_FIELD, PATH, TYPE, BLOCK_EXPR, STMT, PATTERN, EXPR,
         ];
-        let parser_json = r##"[{"body":{"Pub":[16,{"Or":[{"And":[[{"Rep":{"WithSkip":[{"Rule":1},{"Rule":2}]}}],null]}]}]}},{"body":{"Or":[{"And":[[{"Token":10}],null]},{"And":[[{"Token":13}],null]},{"And":[[{"Token":12}],null]}]}},{"body":{"Or":[{"And":[[{"Rule":3}],null]},{"And":[[{"Rule":4}],null]}]}},{"body":{"Pub":[17,{"Or":[{"And":[[{"Opt":{"Or":[{"And":[[{"Token":10}],null]}]}},{"Token":13},{"Token":14},{"Token":2},{"Token":3},{"Rule":7}],2]}]}]}},{"body":{"Pub":[18,{"Or":[{"And":[[{"Opt":{"Token":10}},{"Token":12},{"Token":14},{"Token":4},{"Layer":[{"Rule":8},{"Rep":{"Rule":5}}]},{"Token":5}],2]}]}]}},{"body":{"Pub":[19,{"Or":[{"And":[[{"Opt":{"Token":10}},{"Token":14},{"Token":8},{"Rule":6},{"Or":[{"And":[["Eof"],null]},{"And":[[{"Token":9}],null]}]}],2]}]}]}},{"body":{"Pub":[20,{"Or":[{"And":[[{"Token":14}],null]}]}]}},{"body":{"Pub":[21,{"Or":[{"And":[[{"Token":4},{"Layer":[{"Rule":8},{"Rep":{"Rule":10}}]},{"Token":5}],null]}]}]}},{"body":{"Or":[{"And":[[{"Rep":{"Rule":9}}],null]}]}},{"body":{"Or":[{"And":[[{"Token":4},{"Rule":8},{"Token":5}],1]},{"And":[[{"Not":[5]}],null]}]}},{"body":{"Pub":[22,{"Or":[{"And":[[{"Token":11},{"Rule":11},{"Token":6},{"Rule":12},{"Token":7}],1]}]}]}},{"body":{"Pub":[23,{"Or":[{"And":[[{"Token":14}],null]}]}]}},{"body":{"Pub":[24,{"Or":[{"And":[[{"Token":15}],null]}]}]}}]"##;
+        let parser_json = r##"[{"body":{"Pub":[17,{"Or":[{"And":[[{"Rep":{"WithSkip":[{"Rule":1},{"Rule":2}]}}],null]}]}]}},{"body":{"Or":[{"And":[[{"Token":10}],null]},{"And":[[{"Token":13}],null]},{"And":[[{"Token":12}],null]},{"And":[[{"Token":14}],null]}]}},{"body":{"Or":[{"And":[[{"Rule":4}],null]},{"And":[[{"Rule":5}],null]},{"And":[[{"Rule":3}],null]}]}},{"body":{"Pub":[18,{"Or":[{"And":[[{"Token":14},{"Rule":7},{"Token":7}],null]}]}]}},{"body":{"Pub":[19,{"Or":[{"And":[[{"Opt":{"Or":[{"And":[[{"Token":10}],null]}]}},{"Token":13},{"Token":15},{"Token":2},{"Token":3},{"Rule":9}],2]}]}]}},{"body":{"Pub":[20,{"Or":[{"And":[[{"Opt":{"Token":10}},{"Token":12},{"Token":15},{"Token":4},{"Layer":[{"Rule":10},{"Rep":{"Rule":6}}]},{"Token":5}],2]}]}]}},{"body":{"Pub":[21,{"Or":[{"And":[[{"Opt":{"Token":10}},{"Token":15},{"Token":8},{"Rule":8},{"Or":[{"And":[["Eof"],null]},{"And":[[{"Token":9}],null]}]}],2]}]}]}},{"body":{"Pub":[22,{"Or":[{"And":[[{"Token":15}],null]}]}]}},{"body":{"Pub":[23,{"Or":[{"And":[[{"Token":15}],null]}]}]}},{"body":{"Pub":[24,{"Or":[{"And":[[{"Token":4},{"Layer":[{"Rule":10},{"Rep":{"Rule":12}}]},{"Token":5}],null]}]}]}},{"body":{"Or":[{"And":[[{"Rep":{"Rule":11}}],null]}]}},{"body":{"Or":[{"And":[[{"Token":4},{"Rule":10},{"Token":5}],1]},{"And":[[{"Not":[5]}],null]}]}},{"body":{"Pub":[25,{"Or":[{"And":[[{"Token":11},{"Rule":13},{"Token":6},{"Rule":14},{"Token":7}],1]}]}]}},{"body":{"Pub":[26,{"Or":[{"And":[[{"Token":15}],null]}]}]}},{"body":{"Pub":[27,{"Or":[{"And":[[{"Token":16}],null]}]}]}}]"##;
         let parser: Vec<SynRule> = serde_json::from_str(parser_json).unwrap();
 
         struct Impl { tokenizer: Vec<LexRule>, parser: Vec<SynRule> };
@@ -60,12 +63,15 @@ lazy_static! {
                     KW_LET => NodeTypeInfo { name: "KW_LET" },
                     STRUCT => NodeTypeInfo { name: "STRUCT" },
                     FN => NodeTypeInfo { name: "FN" },
+                    USE => NodeTypeInfo { name: "USE" },
                     IDENT => NodeTypeInfo { name: "IDENT" },
                     NUMBER => NodeTypeInfo { name: "NUMBER" },
                     FILE => NodeTypeInfo { name: "FILE" },
+                    USE_DECL => NodeTypeInfo { name: "USE_DECL" },
                     FN_DEF => NodeTypeInfo { name: "FN_DEF" },
                     STRUCT_DEF => NodeTypeInfo { name: "STRUCT_DEF" },
                     STRUCT_FIELD => NodeTypeInfo { name: "STRUCT_FIELD" },
+                    PATH => NodeTypeInfo { name: "PATH" },
                     TYPE => NodeTypeInfo { name: "TYPE" },
                     BLOCK_EXPR => NodeTypeInfo { name: "BLOCK_EXPR" },
                     STMT => NodeTypeInfo { name: "STMT" },
@@ -90,6 +96,7 @@ lazy_static! {
                 LexRule::new(KW_LET, "let", None),
                 LexRule::new(STRUCT, "struct", None),
                 LexRule::new(FN, "fn", None),
+                LexRule::new(USE, "use", None),
                 LexRule::new(WHITESPACE, "\\s+", None),
                 LexRule::new(IDENT, "\\p{XID_Start}\\w*", None),
                 LexRule::new(NUMBER, "\\d+", None),
