4a1ad4fe3d1a14acd0c239fe68d96c4509cc232e
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-07-09 23:17:43 +0300

    Make self & super into keywords :(

--- lang/rust/tests/data/functions.rs
+++ lang/rust/tests/data/functions.rs
@@ -3,3 +3,8 @@ fn bar(x: i32) {}
 fn baz(x: i32,) {}
 fn quux(x: i32, z: String) {}
 fn ziz() -> foo {}
+
+impl T {
+    fn foo(self) {}
+    fn bar(&self,) {}
+}
--- lang/rust/tests/data/paths.rs
+++ lang/rust/tests/data/paths.rs
@@ -1,4 +1,7 @@
 use foo::bar::baz;
+use self::foo;
+use self::super::super::foo;
+
 
 fn foo() {
     let x = ::Vec::<Vec<i32>>::new;
