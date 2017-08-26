57491dfb1000d566875ce629228ae7c3d5cebff4
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-08-26 18:29:09 +0300

    Add line comments

--- lang/rust/tests/test.rs
+++ lang/rust/tests/test.rs
@@ -3,7 +3,7 @@ extern crate lang_rust;
 
 use std::path::{Path, PathBuf};
 
-use fall_tree::test_util::{check_syntax, check_directory, check_inline_tests};
+use fall_tree::test_util::{check_syntax_ws, check_syntax, check_directory, check_inline_tests};
 use lang_rust::LANG_RUST;
 
 #[test]
@@ -56,4 +56,36 @@ fn check_by_data() {
     let dir = env!("CARGO_MANIFEST_DIR");
     let test_data_path = PathBuf::from(dir).join("tests").join("data");
     check_directory(&LANG_RUST, &test_data_path)
+}
+
+
+#[test]
+fn comments_attachment() {
+    check_syntax_ws(&LANG_RUST, r#"
+/// Doc comment attached
+struct A;
+
+// Simple comment attached
+
+// But only if there are no blank lines
+struct B;
+    "#, r#"
+FILE
+  WHITESPACE "\n"
+  LINE_COMMENT "/// Doc comment attached\n"
+  STRUCT_DEF
+    STRUCT "struct"
+    WHITESPACE " "
+    IDENT "A"
+    SEMI ";"
+  WHITESPACE "\n\n"
+  LINE_COMMENT "// Simple comment attached\n"
+  WHITESPACE "\n"
+  LINE_COMMENT "// But only if there are no blank lines\n"
+  STRUCT_DEF
+    STRUCT "struct"
+    WHITESPACE " "
+    IDENT "B"
+    SEMI ";"
+  WHITESPACE "\n    ""#)
 }
