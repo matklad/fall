dcf8ba7890bed3c816b5048eddbdb4886d408dbe
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-08-27 11:41:10 +0300

    Refactoring

--- fall_parse/src/syn_engine/white.rs
+++ fall_parse/src/syn_engine/white.rs
@@ -19,7 +19,10 @@ pub fn into_white(
         }
     };
 
-    black_to_white(node, tokens, (0, tokens.len()), &binder)
+    Whitespacer {
+        tokens: tokens,
+        whitespace_binder: &binder,
+    }.black_to_white(node, (0, tokens.len()))
 }
 
 
@@ -44,10 +47,6 @@ impl WhiteNode {
         }
 
         WhiteNode { ty, token_range, children }
-    }
-
-    pub fn leaf(ty: Option<NodeType>, token_idx: usize) -> WhiteNode {
-        WhiteNode::new(ty, (token_idx, token_idx + 1), Vec::new())
     }
 
     pub fn into_inode(self, tokens: &[Token]) -> INode {
@@ -80,68 +79,78 @@ impl WhiteNode {
     }
 }
 
+struct Whitespacer<'a> {
+    tokens: &'a [Token],
+    whitespace_binder: &'a Fn(Option<NodeType>, &[Token], bool) -> usize,
+}
 
-fn black_to_white(
-    black: BlackNode,
-    tokens: &[Token],
-    cover_range: (usize, usize),
-    whitespace_binder: &Fn(Option<NodeType>, &[Token], bool) -> usize
-) -> WhiteNode {
-    if let Some((BlackIdx(l), BlackIdx(r))) = black.token_range() {
-        assert!(cover_range.0 <= l && r <= cover_range.1);
+impl<'a> Whitespacer<'a> {
+    fn black_to_white(&self, black: BlackNode, cover_range: (usize, usize)) -> WhiteNode {
+        if let Some((BlackIdx(l), BlackIdx(r))) = black.token_range() {
+            assert!(cover_range.0 <= l && r <= cover_range.1);
+        }
+
+        match black {
+            BlackNode::Leaf { ty, token_idx: BlackIdx(token_idx) } =>
+                return WhiteNode {
+                    ty: ty,
+                    token_range: (token_idx, token_idx + 1),
+                    children: Vec::new(),
+                },
+            BlackNode::Composite { ty, mut children } => {
+                let mut internal_children = Vec::new();
+                children.reverse();
+
+                let mut left_edge = cover_range.0;
+                let mut first_child = true;
+                while let Some(child) = children.pop() {
+                    let right_edge = children.last()
+                        .and_then(|n| n.token_range())
+                        .map(|(_, BlackIdx(i))| i)
+                        .unwrap_or(cover_range.1);
+                    let child = self.black_to_white(child, (left_edge, right_edge));
+
+                    if !first_child {
+                        for i in left_edge..child.token_range.0 {
+                            internal_children.push(self.leaf(i))
+                        }
+                    }
+                    first_child = false;
+                    left_edge = child.token_range.1;
+                    internal_children.push(child);
+                }
+
+                let internal_start = internal_children
+                    .first()
+                    .map(|n| n.token_range.0)
+                    .unwrap_or(cover_range.0);
+                let internal_end = internal_children
+                    .last()
+                    .map(|n| n.token_range.1)
+                    .unwrap_or(cover_range.0);
+
+
+                let external_start = internal_start
+                    - (self.whitespace_binder)(ty, &self.tokens[cover_range.0..internal_start], true);
+
+                let external_end = internal_end
+                    + (self.whitespace_binder)(ty, &self.tokens[internal_end..cover_range.1], false);
+
+                let mut children = Vec::with_capacity(internal_children.len());
+                for i in external_start..internal_start {
+                    children.push(self.leaf(i))
+                }
+                children.extend(internal_children);
+                for i in internal_end..external_end {
+                    children.push(self.leaf(i))
+                }
+                WhiteNode::new(ty, (external_start, external_end), children)
+            }
+        }
     }
 
-    match black {
-        BlackNode::Leaf { ty, token_idx: BlackIdx(token_idx) } =>
-            return WhiteNode::leaf(ty, token_idx),
-        BlackNode::Composite { ty, mut children } => {
-            let mut internal_children = Vec::new();
-            children.reverse();
-
-            let mut left_edge = cover_range.0;
-            let mut first_child = true;
-            while let Some(child) = children.pop() {
-                let right_edge = children.last()
-                    .and_then(|n| n.token_range())
-                    .map(|(_, BlackIdx(i))| i)
-                    .unwrap_or(cover_range.1);
-                let child = black_to_white(child, tokens, (left_edge, right_edge), whitespace_binder);
-
-                if !first_child {
-                    for i in left_edge..child.token_range.0 {
-                        internal_children.push(WhiteNode::leaf(Some(tokens[i].ty), i))
-                    }
-                }
-                first_child = false;
-                left_edge = child.token_range.1;
-                internal_children.push(child);
-            }
-
-            let internal_start = internal_children
-                .first()
-                .map(|n| n.token_range.0)
-                .unwrap_or(cover_range.0);
-            let internal_end = internal_children
-                .last()
-                .map(|n| n.token_range.1)
-                .unwrap_or(cover_range.0);
-
-
-            let external_start = internal_start
-                - whitespace_binder(ty, &tokens[cover_range.0..internal_start], true);
-
-            let external_end = internal_end
-                + whitespace_binder(ty, &tokens[internal_end..cover_range.1], false);
-
-            let mut children = Vec::with_capacity(internal_children.len());
-            for i in external_start..internal_start {
-                children.push(WhiteNode::leaf(Some(tokens[i].ty), i))
-            }
-            children.extend(internal_children);
-            for i in internal_end..external_end {
-                children.push(WhiteNode::leaf(Some(tokens[i].ty), i))
-            }
-            WhiteNode::new(ty, (external_start, external_end), children)
-        }
+    fn leaf(&self, token_idx: usize) -> WhiteNode {
+        let ty = self.tokens[token_idx].ty;
+        WhiteNode::new(Some(ty), (token_idx, token_idx + 1), Vec::new())
     }
 }
