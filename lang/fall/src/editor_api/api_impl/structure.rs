1057d2a6f28b0ee05dc89d787f9222db3824ec51
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-10-25 22:32:39 +0300

    Structure support ast & tokenize

--- lang/fall/src/editor_api/api_impl/structure.rs
+++ lang/fall/src/editor_api/api_impl/structure.rs
@@ -16,6 +16,20 @@ pub fn structure(file: &File) -> Vec<FileStructureNode> {
                 })
             }
         })
+        .visit::<TokenizerDef, _>(|nodes, tokenizer|{
+            nodes.push(FileStructureNode {
+                name: "tokenizer".to_owned(),
+                range: tokenizer.node().range(),
+                children: Vec::new()
+            })
+        })
+        .visit::<AstDef, _>(|nodes, ast|{
+            nodes.push(FileStructureNode {
+                name: "ast".to_owned(),
+                range: ast.node().range(),
+                children: Vec::new()
+            })
+        })
         .walk_recursively_children_first(file.root())
 }
 
@@ -26,11 +40,14 @@ fn test_structure() {
 tokenizer { number r"\d+"}
 pub rule foo { bar }
 rule bar { number }
+ast {
+  node foo { }
+}
 "#);
     let s = structure(&file);
 
     assert_eq!(
         format!("{:?}", s),
-        r#"[FileStructureNode { name: "foo", range: [28; 48), children: [] }, FileStructureNode { name: "bar", range: [49; 68), children: [] }]"#
+        r#"[FileStructureNode { name: "tokenizer", range: [1; 27), children: [] }, FileStructureNode { name: "foo", range: [28; 48), children: [] }, FileStructureNode { name: "bar", range: [49; 68), children: [] }, FileStructureNode { name: "ast", range: [69; 91), children: [] }]"#
     );
 }
