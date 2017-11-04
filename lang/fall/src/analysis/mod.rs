c733424d69abba5c130da1fe4e95fbf5388710e5
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-11-04 23:02:08 +0300

    Move tests

--- lang/fall/src/analysis/mod.rs
+++ lang/fall/src/analysis/mod.rs
@@ -109,8 +109,16 @@ fn analysis_is_sync() {
 #[cfg(test)]
 fn check_diagnostics(code: &str, expected_diagnostics: &str) {
     use editor_api::Severity;
+    use fall_tree::ERROR;
     use fall_tree::test_util::report_diff;
+    use fall_tree::search::subtree;
     let file = ::editor_api::analyse(code.to_string());
+
+    for node in subtree(file.file().root()) {
+        if node.ty() == ERROR {
+            panic!("\nSYNTAX ERROR: {:?}\n\n{}\n\n", node.range(), code)
+        }
+    }
 
     file.analyse(|a| {
         let d = a.collect_all_diagnostics();
@@ -126,20 +134,9 @@ fn check_diagnostics(code: &str, expected_diagnostics: &str) {
     })
 }
 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use analysis::CallKind;
-
-    use fall_tree::search;
-    use fall_tree::search::ast;
-
-    use ::test_util::parse_with_caret;
-
-    #[test]
-    fn test_diagnostics() {
-        check_diagnostics(r"
+#[test]
+fn test_diagnostics() {
+    check_diagnostics(r"
        pub rule foo { <eof x> }
        rule bar { foo <abracadabra>}
        rule baz { <prev_is foo> <prev_is bar> <prev_is {foo}>}
@@ -154,131 +151,4 @@ E <prev_is {foo}>: <prev_is> arguments must be public rules
 E dupe: Duplicate rule
 W baz: Unused rule
 W dupe: Unused rule");
-    }
-
-    #[test]
-    fn test_resolve_no_args() {
-        check_resolved("rule foo { <^eof> }", "Eof");
-        check_resolved("rule foo { <^any> }", "Any");
-        check_resolved("rule foo { <^commit> }", "Commit");
-    }
-
-    #[test]
-    fn test_resolve_simple_args() {
-        check_resolved("rule foo { <^not a> }", "Not(RefExpr@[16; 17))");
-        check_resolved("rule foo { <^with_skip a b> }", "WithSkip(RefExpr@[22; 23), RefExpr@[24; 25))");
-        check_resolved("rule foo { <^layer a b> }", "Layer(RefExpr@[18; 19), RefExpr@[20; 21))");
-    }
-
-    #[test]
-    fn test_resolve_layer() {
-        check_resolved("rule foo { <^enter 'ctx' a> <is_in 'ctx' >}", "Enter(0, RefExpr@[24; 25))");
-        check_resolved("rule foo { <^exit  'ctx' a> <is_in 'ctx' >}", "Exit(0, RefExpr@[24; 25))");
-
-        check_resolved("rule foo { <is_in 'ctx1'> <^enter 'ctx2' a> <is_in 'ctx1'> <enter 'ctx2' a> }", "Enter(1, RefExpr@[40; 41))");
-        check_resolved("rule foo { <is_in 'ctx1'> <^exit  'ctx2' a> <is_in 'ctx1'> <exit  'ctx2' a> }", "Exit(1, RefExpr@[40; 41))");
-
-        check_resolved(
-            "rule foo { <enter 'ctx1' a> <enter 'ctx2' b> <^is_in 'ctx2'> <is_in 'ctx1'> }",
-            "IsIn(1)"
-        );
-
-        check(
-            r" rule foo { <^is_in foo>}",
-            None,
-            r#"[(foo, "Context should be a single quoted string")]"#,
-        )
-    }
-
-    #[test]
-    fn test_not() {
-        resolve_call("rule foo { <^not bar>}", |c, _| {
-            let c = c.unwrap();
-            assert_eq!(format!("{:?}", c), "Not(RefExpr@[16; 19))");
-        })
-    }
-
-    #[test]
-    fn test_unresolved_call() {
-        check(
-            r"rule foo { <^abracadabra>}",
-            None,
-            r#"[(abracadabra, "Unresolved reference")]"#
-        );
-    }
-
-    #[test]
-    fn wrong_arity() {
-        check(
-            "rule foo { <^eof foo> }",
-            Some(CallKind::Eof),
-            r#"[(<eof foo>, "Wrong number of arguments, expected 0, got 1")]"#
-        );
-
-        check(
-            "rule foo { <^any foo bar> }",
-            Some(CallKind::Any),
-            r#"[(<any foo bar>, "Wrong number of arguments, expected 0, got 2")]"#
-        );
-
-        check(
-            "rule foo { <^commit foo bar baz> }",
-            Some(CallKind::Commit),
-            r#"[(<commit foo bar baz>, "Wrong number of arguments, expected 0, got 3")]"#
-        );
-
-        check(
-            "rule foo { <^not> }",
-            None,
-            r#"[(<not>, "Wrong number of arguments, expected 1, got 0")]"#
-        );
-    }
-
-    #[test]
-    fn check_prev_is() {
-        check_resolved(
-            "pub rule foo {} pub rule bar {} rule baz { <^prev_is foo bar> }",
-            "PrevIs([1, 2])"
-        )
-    }
-
-    #[test]
-    fn call_custom_rule() {
-        check_resolved(
-            "rule foo { <^bar a b>} rule bar(x, y) { }",
-            "RuleCall(SynRule@[22; 40), [(0, RefExpr@[16; 17)), (1, RefExpr@[18; 19))])"
-        );
-        ::analysis::check_diagnostics(
-            "rule foo { <bar <eof>>} rule bar(x, y) { }",
-            "E <bar <eof>>: Expected 2 arguments, got 1"
-        )
-    }
-
-    fn check(text: &str, kind: Option<CallKind>, diagnostics: &str) {
-        resolve_call(text, |c, a| {
-            assert_eq!(c, kind);
-            assert_eq!(a.debug_diagnostics(), diagnostics);
-        })
-    }
-
-    fn check_resolved(text: &str, kind: &str) {
-        resolve_call(text, |c, a| {
-            let c = c.unwrap();
-            assert_eq!(format!("{:?}", c), kind);
-            assert_eq!(a.debug_diagnostics(), "[]");
-        })
-    }
-
-    fn resolve_call<F: FnOnce(Option<CallKind>, Analysis)>(text: &str, f: F) {
-        let (file, caret) = parse_with_caret(&text);
-        let call = {
-            let leaf = search::find_leaf_at_offset(file.root(), caret)
-                .left_biased().unwrap();
-            ast::ancestor_exn(leaf)
-        };
-        let a = Analysis::new(::ast(&file));
-        let c = a.resolve_call(call);
-        f(c, a)
-    }
 }
-
--- lang/fall/src/analysis/query/resolve_call.rs
+++ lang/fall/src/analysis/query/resolve_call.rs
@@ -133,3 +133,119 @@ fn resolve_context(db: &DB, d: &mut DiagnosticSink, call: CallExpr) -> Option<u3
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use fall_tree::search::find_leaf_at_offset;
+    use fall_tree::search::ast;
+    use analysis::*;
+    use test_util::parse_with_caret;
+
+    #[test]
+    fn test_resolve_no_args() {
+        check_resolved("rule foo { <^eof> }", "Eof");
+        check_resolved("rule foo { <^any> }", "Any");
+        check_resolved("rule foo { <^commit> }", "Commit");
+    }
+
+    #[test]
+    fn test_resolve_simple_args() {
+        check_resolved("rule foo { <^not a> }", "Not(RefExpr@[16; 17))");
+        check_resolved("rule foo { <^with_skip a b> }", "WithSkip(RefExpr@[22; 23), RefExpr@[24; 25))");
+        check_resolved("rule foo { <^layer a b> }", "Layer(RefExpr@[18; 19), RefExpr@[20; 21))");
+    }
+
+    #[test]
+    fn test_resolve_layer() {
+        check_resolved("rule foo { <^enter 'ctx' a> }", "Enter(0, RefExpr@[24; 25))");
+        check_resolved("rule foo { <^exit  'ctx' a> }", "Exit(0, RefExpr@[24; 25))");
+
+        check_resolved("rule foo { <is_in 'ctx1'> <^enter 'ctx2' a>  }", "Enter(1, RefExpr@[40; 41))");
+        check_resolved("rule foo { <is_in 'ctx1'> <^exit  'ctx2' a>  }", "Exit(1, RefExpr@[40; 41))");
+
+        check_resolved(
+            "rule foo { <enter 'ctx1' a> <enter 'ctx2' b> <^is_in 'ctx2'> }",
+            "IsIn(1)"
+        );
+
+        check_diagnostics(
+            "rule foo { <is_in foo>}",
+            "E foo: Context should be a single quoted string"
+        );
+    }
+
+    #[test]
+    fn test_not() {
+        check_resolved("rule foo { <^not bar>}", "Not(RefExpr@[16; 19))")
+    }
+
+    #[test]
+    fn test_unresolved_call() {
+        check_diagnostics(
+            "rule foo { <abracadabra> foo}",
+            "E abracadabra: Unresolved reference"
+        );
+    }
+
+    #[test]
+    fn wrong_arity() {
+        check_diagnostics(
+            "rule foo { <eof foo> }",
+            r#"E <eof foo>: Wrong number of arguments, expected 0, got 1"#
+        );
+
+        check_diagnostics(
+            "rule foo { <any foo foo> }",
+            "E <any foo foo>: Wrong number of arguments, expected 0, got 2"
+        );
+
+        check_diagnostics(
+            "rule foo { <commit foo foo foo> }",
+            "E <commit foo foo foo>: Wrong number of arguments, expected 0, got 3"
+        );
+
+        check_diagnostics(
+            "rule foo { <not> foo }",
+            "E <not>: Wrong number of arguments, expected 1, got 0"
+        );
+    }
+
+    #[test]
+    fn check_prev_is() {
+        check_resolved(
+            "pub rule foo {} pub rule bar {} rule baz { <^prev_is foo bar> }",
+            "PrevIs([1, 2])"
+        )
+    }
+
+    #[test]
+    fn call_custom_rule() {
+        check_resolved(
+            "rule foo { <^bar a b>} rule bar(x, y) { }",
+            "RuleCall(SynRule@[22; 40), [(0, RefExpr@[16; 17)), (1, RefExpr@[18; 19))])"
+        );
+        check_diagnostics(
+            "rule foo { <bar <eof>>} rule bar(x, y) { }",
+            "E <bar <eof>>: Expected 2 arguments, got 1"
+        );
+    }
+
+    fn check_resolved(text: &str, kind: &str) {
+        resolve_call(text, |c, _| {
+            let c = c.unwrap();
+            assert_eq!(format!("{:?}", c), kind);
+        })
+    }
+
+    fn resolve_call<F: FnOnce(Option<CallKind>, Analysis)>(text: &str, f: F) {
+        let (file, caret) = parse_with_caret(&text);
+        let call = {
+            let leaf = find_leaf_at_offset(file.root(), caret)
+                .left_biased().unwrap();
+            ast::ancestor_exn(leaf)
+        };
+        let a = Analysis::new(::ast(&file));
+        let c = a.resolve_call(call);
+        f(c, a)
+    }
+}
